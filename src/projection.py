af8b7f96a0d0a1779b7d7ab19d21849c3c3ac236
Roelof Ruis <roelof@branchonline.nl>
2014-10-31 10:26:09 +0100

    fixing gitignore

--- src/projection.py
+++ src/projection.py
@@ -1,17 +1,18 @@
 from __future__ import division
 from calc import Matrix
+from canvas import Screen
 
 class Camera():
-    def __init__(self, screen):
+    def __init__(self):
         self.internal = Matrix(3,3).setTo([
-            [1,0,200],
-            [0,1,200],
+            [100,0,200],
+            [0,100,200],
             [0,0,1]])
         self.external = Matrix(3,4).setTo([
-            [0.5,0,0,10],
-            [0,0.5,0,10],
-            [0,0,1,-2]])
-        self.screen = screen
+            [1,0,0.5,0],
+            [0,1,-0.2,0],
+            [0,0,1,1]])
+        self.screen = Screen()
 
     def __str__(self):
         return ('Camera (\n M_int %s\n M_ext %s\n)' % (self.internal, self.external))
@@ -21,7 +22,6 @@ class Camera():
 
     def _transformPoint(self, point):
         proj = self.external.multiply(point.getHomCoords())
-        print(proj)
         proj = self.internal.multiply(proj)
         w = proj.getElem(2,0)
         if w == 0:
@@ -36,4 +36,9 @@ class Camera():
         pt1 = self._transformPoint(pts[0])
         pt2 = self._transformPoint(pts[1])
         self.screen.drawLine(pt1, pt2)
+
+    def lookAt(self, world):
+        for shape in world.getShapes():
+            shape.project(self)
+        self.screen.show()
         
